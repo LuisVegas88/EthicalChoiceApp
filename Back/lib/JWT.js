9c163282a3105df7adba9f9297d636d7eac7e36f
LuisVegas88 <71334475+LuisVegas88@users.noreply.github.com>
2020-12-22 16:05:16 +0100

    Añadir Favorito

---
+++ Back/lib/JWT.js
@@ -0,0 +1,66 @@
+const crypto = require("crypto");
+const base64 = require("base-64");
+const dotenv = require("dotenv").config();
+const SECRET = process.env.SECRET_JWT;
+
+
+
+function encodeBase64(string) {
+	const encodedString = base64.encode(string);
+	const parsedString = encodedString
+		.replace(/=/g, "")
+		.replace(/\+/g, "-")
+		.replace(/\//g, "_");
+	return parsedString;
+}
+
+function decodeBase64(base64String) {
+	const decodedString = base64.decode(base64String);
+	return decodedString;
+}
+
+function generateJWT(Payload) {
+	const header = {
+		"alg": "HS256",
+		"typ": "JWT"
+	};
+	const base64Payload = encodeBase64(JSON.stringify(Payload));
+	const base64Header = encodeBase64(JSON.stringify(header));
+	const signature = encodeBase64(hash(`${base64Header}.${base64Payload}`));
+	const JWT = `${base64Header}.${base64Payload}.${signature}`;
+	return JWT;
+}
+
+function hash(string, Key = SECRET) {
+	const hashedString = crypto
+		.createHmac("sha256", Key)
+		.update(string)
+		.digest("base64");
+	return hashedString;
+}
+
+function verifyJWT(jwt) {
+	const [header, payload, signature] = jwt.split(".");
+	if (header && payload && signature) {
+		const expectedSignature = encodeBase64(hash(`${header}.${payload}`));
+		if (expectedSignature === signature) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function getJWTInfo(jwt) {
+	const payload = jwt.split(".")[1];
+	if (payload) {
+		try {
+			const data = JSON.parse(decodeBase64(payload));
+			return data;
+		} catch (e) {
+			return null;
+		}
+	}
+	return null;
+}
+
+module.exports = {"generateJWT" : generateJWT, "verifyJWT" : verifyJWT, "getJWTInfo" : getJWTInfo, hash};
--- Back/server.js
+++ Back/server.js
@@ -17,10 +17,11 @@ const options = {
 
 	"maxAge": 1000 * 60 * 15 * 4 * 24 * 15, // would expire after 15 days		////// OPTIONS DE JWT//////
 	"httpOnly": true, // The cookie only accessible by the web server
-	"signed": true // Indicates if the cookie should be signed
+	"signed": false // Indicates if the cookie should be signed
 };
 const Facebook = require("./lib/OauthFacebook");
 const facebook = new Facebook();
+const JWT = require("./lib/JWT.js");
 
 //////////MiddlewaresServer//////////////
 server.use(myPublicFiles);
@@ -29,7 +30,7 @@ server.use(cors());
 
 ////Others Middlewares/////////
 server.use(cors());
-server.use(cookieParser(SECRET));
+server.use(cookieParser());
 server.use(bodyParser.json());
 
 ////////////////////////////////////////////VALIDATORS//////////////////////////////////////////////////////
@@ -53,82 +54,26 @@ function CredentialsValidator(Email, Password) {
 // const SECRET = crypto.randomBytes(80).toString("hex");
 
 ////COMPROBACIÓN DEL JWT/////
-server.get("/jwt", (req, res) => {
+// server.get("/jwt", (req, res) => {
 
-	const Payload = {
+// 	const Payload = {
 
-		"userName": "Admin",
-		"iat": new Date(),
-		"role": "Admin",
-		"ip": req.ip
-	};
-	const JWT = generateJWT(Payload);
-	res.cookie("jwt", JWT, { "httpOnly": true });
-	res.send("Hola Mundo");
-});
+// 		"userName": "Admin",
+// 		"iat": new Date(),
+// 		"role": "Admin",
+// 		"ip": req.ip
+// 	};
+// 	const JWT = generateJWT(Payload);
+// 	res.cookie("jwt", JWT, { "httpOnly": true });
+// 	res.send("Hola Mundo");
+// });
 
 /////todo OK¡(Comprobado)
 
 //FUNCIONES PARA CODIFICACION JWT  (front-end)
 
 
-function encodeBase64(string) {
-	const encodedString = base64.encode(string);
-	const parsedString = encodedString
-		.replace(/=/g, "")
-		.replace(/\+/g, "-")
-		.replace(/\//g, "_");
-	return parsedString;
-}
 
-function decodeBase64(base64String) {
-	const decodedString = base64.decoded(base64String);
-	return decodedString;
-}
-
-function generateJWT(Payload) {
-	const header = {
-		"alg": "HS256",
-		"typ": "JWT"
-	};
-	const base64Payload = encodeBase64(JSON.stringify(Payload));
-	const base64Header = encodeBase64(JSON.stringify(header));
-	const signature = encodeBase64(hash(`${base64Header}.${base64Payload}`));
-	const JWT = `${base64Header}.${base64Payload}.${signature}`;
-	return JWT;
-}
-
-function hash(string) {
-	const hashedString = crypto
-		.createHmac("sha256", SECRET)
-		.update(string)
-		.digest("base64");
-	return hashedString;
-}
-
-function verifyJWT(jwt) {
-	const [header, payload, signature] = jwt.split(".");
-	if (header && payload && signature) {
-		const expectedSignature = encodeBase64(hash(`${header}.${payload}`));
-		if (expectedSignature === signature) {
-			return true;
-		}
-	}
-	return false;
-}
-
-function getJWTInfo(jwt) {
-	const [payload] = jwt.split(".")[1];
-	if (payload) {
-		try {
-			const data = JSON.parse(decodeBase64(payload));
-			return data;
-		} catch (e) {
-			return null;
-		}
-	}
-	return null;
-}
 
 //////////////////////////////////////////////////////////////////OAUTHs/////////////////////////////////////////////////////
 /////////////////GOOGLE///////////////////////////
@@ -163,7 +108,7 @@ server.get("/loginG", async (req, res) => {
 									if (err)
 										connection.query(`DELETE FROM User WHERE idUser = ? `, [idUser])
 									const Payload = {
-										"id": idUser,
+										"idUser": idUser,
 										"User": name,
 										"Email": email,
 										"iat": new Date(),
@@ -378,7 +323,7 @@ server.post("/Login", (req, res) => {
 				throw err;
 			if (result.length) {
 				let idUser = result[0].idUser;
-				connection.query(`SELECT * FROM UserRegister WHERE Hashpass = ? AND idUser=?`, [hash(userLogin.HashPass), idUser], (err, result) => {
+				connection.query(`SELECT * FROM UserRegister WHERE Hashpass = ? AND idUser=?`, [JWT.hash(userLogin.HashPass), idUser], (err, result) => {
 					if (err)
 						throw err;
 					if (result.length)
@@ -390,7 +335,7 @@ server.post("/Login", (req, res) => {
 							"role": "User",
 							"ip": req.ip
 						};
-						res.cookie("jwt", generateJWT(Payload), options).send({ "msg": "Logged¡" });
+						res.cookie("jwt", JWT.generateJWT(Payload), options).send({ "msg": "Logged¡" });
 					} else {
 						res.send("Wrong credentials")
 					}
@@ -570,7 +515,7 @@ server.put("/User/Edit/", (req, res) => {
 		connection.end();
 	}
 })
-
+///////////////////////////////////////////////////////F A V S///////////////////////////////////////////////////////////////////////
 ///U SER'S FAVS LIST //
 
 server.get("/Favs", (req, res) => {
@@ -586,7 +531,35 @@ server.get("/Favs", (req, res) => {
 	})
 	connection.end();
 })
+///Add FAV///
 
+server.put("/AddFav", (req, res) =>{
+	if (JWT.verifyJWT(req.cookies.jwt))
+	{
+		const {idUser} = JWT.getJWTInfo(req.cookies.jwt)
+		if (idUser)
+		{
+			console.log(idUser);
+			const {idProduct} = req.query;
+				if(idProduct,idUser){
+					let connection = openDB();
+					connection.query(`INSERT INTO Favs (idUser, idProduct) VALUES (?,?)`,[idUser,idProduct], (err,result)=>{
+						if(err){
+							res.send(err);
+						}
+						if(result){
+							res.send({msg:0,tst:"Inserted Fav"});
+						}
+					})
+				connection.end();	
+			}
+		}
+		else
+			res.send({"error": "No JWT"})
+	}
+	else
+		res.send({"error": "No JWT"})
+})
 ///Delete FAV ////
 
 server.get("/DeleteFav", (req, res) => {
@@ -652,7 +625,11 @@ server.get("/DeleteFolder", (req, res) => {
 	})
 	connection.end();
 })
+///CREATE FOLDER///
 
+server.put("/CreateFolder", (req , res) =>{
+
+} )
 ///DELETE FAV FROM USER'S CONTENT///
 
 server.get("/DeleteFolderContent", (req, res) => {
